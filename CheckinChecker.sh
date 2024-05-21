0f0a7e111a6d27a86d5e15fa572364d6e778b9ae
Brooke Burdick <36173452+broojamfburd@users.noreply.github.com>
2024-05-21 15:48:15 -0700

    Update CheckinChecker.sh
    Added error handing/ more verbose error output. Handling no jamf binary, no logs, added loop to try to fix binary.

--- CheckinChecker.sh
+++ CheckinChecker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Part 3 of 4 of CheckinChecker Program, main script
+# Part 3/4 CheckinChecker Program
 # Created by: Brooke Burdick brooburd@gmail.com
 # v1 2024
 ###THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.###
@@ -36,14 +36,14 @@ CheckForNetwork(){
 }
 
 #Not used unless using an on-premise server
-CheckSiteNetwork (){  
-  site_network="False"
-  ping=`host -W .5 $jss_server_address`
+#CheckSiteNetwork (){  
+#  site_network="False"
+#  ping=`host -W .5 $jss_server_address`
   
   # If the ping fails - site_network="False"
-  [[ $? -eq 0 ]] && site_network="True"
+#  [[ $? -eq 0 ]] && site_network="True"
   
-}
+#}
 
 # Identify location of jamf binary.
 CheckBinary (){
@@ -63,71 +63,83 @@ jamf_binary=`/usr/bin/which jamf`
 #output as Month day, no year in the logs, so we have to use this year by default...
 #If it has been longer than 90 days and all other network checks succeed, proceed to restart Jamf Binary or prompt user to reinstall
 LastCheckinDay () {
-  line=$(grep "recurring check-in" /private/var/log/jamf.log | tail -1 )
-  lastCheckin=$(echo $line | awk '{print $2, $3, $4 }')
-  ScriptLogging "Last Checkin was $lastCheckin"
+#Before checking last check-in, e need to make sure there have been any check-ins
+  stg='recurring check-in'
+  ln=$(grep "recurring check-in" /private/var/log/jamf.log)e
+  if echo -- '%s' "$ln" | egrep -q -- "$stg"
+  then
+    line=$(grep "recurring check-in" /private/var/log/jamf.log | tail -1 )
+    lastCheckin=$(echo $line | awk '{print $2, $3, $4 }')
+    ScriptLogging "Last Checkin was $lastCheckin"
   
-  formatDate=$(echo $lastCheckin )
+    formatDate=$(echo $lastCheckin )
   
-  ### Validate input
-  ### Read input
-  monStr=$( echo $lastCheckin | awk '{print $1}' )
-  dayNum=$( echo $lastCheckin | awk '{print $2}' )
-  timeNum=$( echo $lastCheckin | awk '{print $3}')
-  hrs=$( echo $timeNum | awk '{print $1}' )
-  mins=$( echo $timeNum | awk '{print $2}' )
-  secs=$( echo $timeNum | awk '{print $3}' )
-  yearNum=$(date +%Y )
-  if [[ "$monStr" = "Jan" ]]; then
-    monNum=01
-  elif [[ "$monStr" = "Feb" ]]; then
-    monNum=02
-  elif [[ "$monStr" = "Mar" ]]; then
-    monNum=03
-  elif [[ "$monStr" = "Apr" ]]; then
-    monNum=04
-  elif [[ "$monStr" == "May" ]]; then
-    monNum=05
-  elif [[ "$monStr" = "Jun" ]]; then
-    monNum=06
-  elif [[ "$monStr" = "Jul" ]]; then
-    monNum=07
-  elif [[ "$monStr" = "Aug" ]]; then
-    monNum=08
-  elif [[ "$monStr" = "Sep" ]]; then
-    monNum=09
-  elif [[ "$monStr" = "Oct" ]]; then
-    monNum=10
-  elif [[ "$monStr" = "Nov" ]]; then
-    monNum=11
-  elif [[ "$monStr" == "Dec" ]]; then
-    monNum=12
-  fi
-  checkinNum=$(echo $yearNum-$monNum-$dayNum )
-  #echo "Last Checkin date was $checkinNum"
-  
-  #convert last checkin to epoch time
-  lastCheckinEpoch=$( date -j -f "%Y-%m-%d" "$checkinNum" "+%s")
-  #echo "Last checkin epoch" $lastCheckinEpoch 
-  todayEpoch=$( date $today "+%s")
-  #echo "Today epoch" $todayEpoch 
-  
-  elapsedTime=$(($todayEpoch - $lastCheckinEpoch))
-  ScriptLogging "Elapsed Time since last checkin (in seconds) $elapsedTime"
+    ### Validate input
+    ### Read input
+    monStr=$( echo $lastCheckin | awk '{print $1}' )
+    dayNum=$( echo $lastCheckin | awk '{print $2}' )
+    timeNum=$( echo $lastCheckin | awk '{print $3}')
+    hrs=$( echo $timeNum | awk '{print $1}' )
+    mins=$( echo $timeNum | awk '{print $2}' )
+    secs=$( echo $timeNum | awk '{print $3}' )
+    yearNum=$(date +%Y )
+    if [[ "$monStr" = "Jan" ]]; then
+      monNum=01
+    elif [[ "$monStr" = "Feb" ]]; then
+      monNum=02
+    elif [[ "$monStr" = "Mar" ]]; then
+      monNum=03
+    elif [[ "$monStr" = "Apr" ]]; then
+      monNum=04
+    elif [[ "$monStr" == "May" ]]; then
+      monNum=05
+    elif [[ "$monStr" = "Jun" ]]; then
+      monNum=06
+    elif [[ "$monStr" = "Jul" ]]; then
+      monNum=07
+    elif [[ "$monStr" = "Aug" ]]; then
+      monNum=08
+    elif [[ "$monStr" = "Sep" ]]; then
+      monNum=09
+    elif [[ "$monStr" = "Oct" ]]; then
+      monNum=10
+    elif [[ "$monStr" = "Nov" ]]; then
+      monNum=11
+    elif [[ "$monStr" == "Dec" ]]; then
+      monNum=12
+    fi
+    checkinNum=$(echo $yearNum-$monNum-$dayNum )
+    #echo "Last Checkin date was $checkinNum"
+      #convert last checkin to epoch time
+      lastCheckinEpoch=$( date -j -f "%Y-%m-%d" "$checkinNum" "+%s")
+      #echo "Last checkin epoch" $lastCheckinEpoch 
+      todayEpoch=$( date $today "+%s")
+      #echo "Today epoch" $todayEpoch 
+      elapsedTime=$(($todayEpoch - $lastCheckinEpoch))
+  # If the logs do not exist or there are no check-ins in the log, it will exit and prompt the user to contact support
+  else 
+    ScriptLogging "Jamf Checkin/ Log Not Found, Prompting user to call support"
+    ScriptLogging "********************* EXITING CHECKIN CHECKER - NO CHECKINS LOGGED ********************"
+    checkinCheckerDaemon
+    exit 0
+  fi 
 }
 
 #Jamf recon
 forceCheckin(){
   ScriptLogging "Running Recon"
-  sudo $jamf_binary recon >> $log_location
-  sleep 20
+  sudo $jamf_binary recon  >> $log_location
+  #sudo $jamf_binary policy  >> $log_location
+  sleep 10
 }
 
 #Restart Jamf binary
 restartBinary(){
+  ScriptLogging "Restarting Jamf Binary..."
   sudo killall jamf
   sleep 10
-  sudo jamf policy
+  ScriptLogging "Running Recon"
+  sudo $jamf_binary recon
 }
 
 checkinCheckerDaemon(){
@@ -155,11 +167,15 @@ checkinCheckerDaemon(){
 }
 
 deleteCheckerDaemon (){
-  sudo launchctl bootout system/com.checkincheckerprompt
+  if [[ -f "/private/tmp/CheckinChecker/checkincheckerprompt.plist" ]]; then
+    sudo launchctl bootout system/com.checkincheckerprompt
+    rm -f /private/tmp/CheckinChecker/checkincheckerprompt.plist
+    ScriptLogging "Removed checkincheckerprompt.plist, User will receive no more prompts"
+  fi
 }
 #End Functions
 
-ScriptLogging "******************** Starting CheckinChecker ********************"
+ScriptLogging "******************** STARTING CHECKIN CHECKER ********************"
 # Wait up to 60 minutes for a network connection to become 
 # available which doesn't use a loopback address. This 
 # condition which may occur if this script is run by a 
@@ -184,7 +200,7 @@ done
 # the script will exit.
 
 if [[ "${NETWORKUP}" != "-YES-" ]]; then
-   ScriptLogging "Network connection appears to be offline. Exiting."
+   ScriptLogging "********************* EXITING CHECKIN CHECKER - NO NETWORK ********************"
     exit 0
 elif [[ "${NETWORKUP}" == "-YES-" ]]; then
    ScriptLogging "Network connection appears to be live."
@@ -209,45 +225,51 @@ if [[ $jamf_binary == "/usr/sbin/jamf" ]]; then
 elif [[ $jamf_binary == "/usr/local/bin/jamf" ]]; then
   ScriptLogging "Jamf Binary found at $jamf_binary"
 else
-    ScriptLogging "Jamf Binary Not Installed"
+    ScriptLogging "Jamf Binary Not Installed, Prompting user to call support"
+    ScriptLogging "********************* EXITING CHECKING CHECKER - NO JAMF BINARY ********************"
     checkinCheckerDaemon
+    exit 0
 fi  
 
 #CHECK LAST CHECKIN DATE
 #If it's been over 90 days, it will attempt to fix binary, and then a launchdaemon will be created to run a prompt every X minutes to call support
+
 ScriptLogging "Checking last checkin day."
 LastCheckinDay
-#For testing
-#elapsedTime=7776001
+#For testing elapsedTime=7777777
+
+echo "Last Checkin date was $checkinNum"
 if [[ $elapsedTime == 0 ]]; then
   ScriptLogging "Device checked in today."
-  deleteCheckerDaemon > /dev/null
-#If it's been longer than 90 days, attempt to force check in and restart the binary
-#If it still is not checking in, then launch the daemon  
-elif [[ $elapsedTime > 7776000 ]]; then
-  ScriptLogging "Device has not checked in in over 90 days. Elapsed Time is $elapsedTime (in seconds)."
+  deleteCheckerDaemon > $log_location
+  #If it's been longer than 90 days, attempt to force check in and restart the binary
+  #If it still is not checking in, then launch the daemon  
+elif [[ $elapsedTime > 7776001 ]]; then
+  ScriptLogging "Device has not checked in in over 90 days. Elapsed Time is $elapsedTime (in seconds). Last Checkin was $lastCheckin"
   ScriptLogging "Attempting to fix Jamf Binary."
   restartBinary 
-  forceCheckin 
   sleep 10
   LastCheckinDay
-    if [[ $elapsedTime == 0 ]]; then
-      ScriptLogging "Device checked in today."
-    elif [[ $elapsedTime > 7776000 ]]; then
-      ScriptLogging "Device has not checked in in over 90 days. Elapsed Time is $elapsedTime (in seconds)."
-      ScriptLogging "Creating LaunchDaemon com.checkincheckerprompt."
-      checkinCheckerDaemon
-    elif [[ $elapsedTime < 7776000 ]]; then
-      ScriptLogging "Device has not checked in in today. Elapsed time is $elapsedTime. Last checkin was $checkinNum."
-    else
-      ScriptLogging "Unable to calculate last checkin. Exiting."
-    fi
+  if [[ $elapsedTime == 0 ]]; then
+    ScriptLogging "Device checked in today."
+  elif [[ $elapsedTime > 7776000 ]]; then
+    ScriptLogging "Device has not checked in in over 90 days. Last checkin was $lastCheckin."
+    ScriptLogging "Creating LaunchDaemon com.checkincheckerprompt."
+    checkinCheckerDaemon
+  elif [[ $elapsedTime < 7776000 ]]; then
+    ScriptLogging "Device has not checked in in today. Last checkin was $lastCheckin."
+  else
+    ScriptLogging "Unable to calculate last checkin. Exiting."
+    ScriptLogging "********************* EXITING CHECKING CHECKER - NO CHECKIN DATE ********************"
+  fi
 elif [[ $elapsedTime < 7776000 ]]; then
-  ScriptLogging "Device has not checked in in today. Elapsed time is $elapsedTime. Last checkin was $checkinNum."
-  deleteCheckerDaemon > log_location
+  ScriptLogging "Last checkin was $lastCheckin."
+  deleteCheckerDaemon > $log_location
 else
   ScriptLogging "Unable to calculate last checkin. Exiting."
+  ScriptLogging "********************* EXITING CHECKING CHECKER - NO CHECKIN DATE ********************"
+  exit 0
 fi	
 
-ScriptLogging "******************** CheckinChecker Complete ********************"
+ScriptLogging "******************** CHECKIN CHECKER COMPLETE ********************"
 exit 0
