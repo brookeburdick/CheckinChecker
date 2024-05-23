1a2d139ca4d73e266fe4cdd96c47f5d92bb3a858
Brooke Burdick <36173452+broojamfburd@users.noreply.github.com>
2024-05-23 14:36:50 -0700

    Update CheckinCheckerPrompt.sh

--- CheckinCheckerPrompt.sh
+++ CheckinCheckerPrompt.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# Part of 4 of 4 the CheckinChecker Program
-# This script creates a pop-up on the user's screen
-# The associated Launchdaemon will call this script every 5 minutes
-# To customize the pop-up, you can edit the AppleScript
-# Created by: Brooke Burdick brooburd@gmail.com
+# This script will kick off only if it has been over 90 days since last checkin
+# This can be customized if you wish to change or add to the pop-up message/ icon
 # v1 2024
 ###THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.###
 
@@ -24,4 +21,4 @@ ScriptLogging(){
 #Ask User to Contact Support via pop-up, LaunchDaemon will run this at intervals
 ScriptLogging "Prompting User to Contact support to re-enroll machine."
 osascript <<EOD
-	display dialog "Your Mac is no longer protected by MDM security software. \n\nPlease save your work and contact IT Support immediately to re-enroll your Mac." with title "Immediate Action Required" with icon posix file "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertStopIcon.icns" buttons {"I Understand"} default button 1
+	display dialog "Your Mac is no longer protected by Parachute security software. \n\nPlease save your work and contact Parachute Support immediately to re-enroll your Mac.\n\nTelephone: 415-762-0720\nEmail: Support@parachutetechs.com" with title "Immediate Action Required" with icon posix file "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertStopIcon.icns" buttons {"I Understand"} default button 1
