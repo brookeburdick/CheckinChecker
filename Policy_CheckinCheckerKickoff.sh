c6b419a10f20e158935c3c58b5fc51786f00c9ff
Brooke Burdick <36173452+broojamfburd@users.noreply.github.com>
2024-05-28 13:55:06 -0700

    Update Policy_CheckinCheckerKickoff.sh
    Changed interval to 600 for testing.

--- Policy_CheckinCheckerKickoff.sh
+++ Policy_CheckinCheckerKickoff.sh
@@ -18,7 +18,7 @@
     <key>RunAtLoad</key>
     <true/>
     <key>StartInterval</key>
-    <integer>60</integer> 
+    <integer>600</integer> 
   </dict>
   </plist>" > ~/Library/LaunchAgents/com.checkinchecker.plist
   
